c20b5e72a1305538357a0870e837926a9367dc73
reinol martin <reinolmartin01@gmail.com>
2025-05-20 17:19:29 +0300

    new changes: modified polyfills.js

---
+++ polyfills.js
@@ -0,0 +1,6 @@
+// Fix for touch events in React Native Web
+if (typeof document !== 'undefined') {
+  document.addEventListener('touchstart', function() {}, { passive: true });
+  document.addEventListener('touchmove', function() {}, { passive: true });
+  document.addEventListener('touchend', function() {}, { passive: true });
+}
