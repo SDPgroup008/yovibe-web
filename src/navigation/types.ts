56466af44745023c61bf5b3380154d1c6084d71f
reinol martin <reinolmartin01@gmail.com>
2025-06-27 05:44:02 +0300

    new changes: modified types.ts

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,44 +1,31 @@
-import type { NativeStackScreenProps } from "@react-navigation/native-stack"
-import type { Event } from "../models/Event"
-
-// Auth Stack
-export type AuthStackParamList = {
-  Login: undefined
-  SignUp: undefined
-}
-
-// Venues Stack
 export type VenuesStackParamList = {
   VenuesList: undefined
   VenueDetail: { venueId: string }
   AddEvent: { venueId: string; venueName: string }
   EventDetail: { eventId: string }
-  ManagePrograms: { venueId: string; weeklyPrograms: Record<string, string> }
+  ManagePrograms: { venueId: string; venueName: string }
   TodaysVibe: { venueId: string; venueName: string }
   TicketPurchase: { event: Event }
   TicketScanner: { eventId: string }
 }
 
-// Events Stack
 export type EventsStackParamList = {
   EventsList: undefined
   EventDetail: { eventId: string }
-  AddEvent: { venueId?: string; venueName?: string }
+  AddEvent: undefined
   VenueDetail: { venueId: string }
   TicketPurchase: { event: Event }
   TicketScanner: { eventId: string }
 }
 
-// Map Stack
 export type MapStackParamList = {
-  MapView: { destinationVenueId?: string }
+  MapView: undefined
   VenueDetail: { venueId: string }
   EventDetail: { eventId: string }
   TicketPurchase: { event: Event }
   TicketScanner: { eventId: string }
 }
 
-// Calendar Stack
 export type CalendarStackParamList = {
   CalendarView: undefined
   EventDetail: { eventId: string }
@@ -47,7 +34,6 @@ export type CalendarStackParamList = {
   TicketScanner: { eventId: string }
 }
 
-// Profile Stack
 export type ProfileStackParamList = {
   ProfileMain: undefined
   MyVenues: undefined
@@ -63,74 +49,15 @@ export type ProfileStackParamList = {
   TicketScanner: { eventId: string }
 }
 
-// Main Tab
+export type AuthStackParamList = {
+  Login: undefined
+  SignUp: undefined
+}
+
 export type MainTabParamList = {
   Venues: undefined
   Events: undefined
   Map: undefined
   Calendar: undefined
   Profile: undefined
-}
-
-// Screen Props - These were missing!
-export type ProfileScreenProps = {
-  navigation: any
-  route?: any
-}
-
-export type EventDetailScreenProps = NativeStackScreenProps<EventsStackParamList, "EventDetail"> & {
-  navigation: any
-}
-
-export type EventsScreenProps = {
-  navigation: any
-  route?: any
-}
-
-export type MapScreenProps = NativeStackScreenProps<MapStackParamList, "MapView"> & {
-  navigation: any
-}
-
-export type VenueDetailScreenProps = NativeStackScreenProps<VenuesStackParamList, "VenueDetail"> & {
-  navigation: any
-}
-
-// Ticket Screen Props
-export type TicketPurchaseScreenProps = {
-  route: {
-    params: {
-      event: Event
-    }
-  }
-  navigation: any
-}
-
-export type TicketScannerScreenProps = {
-  route: {
-    params: {
-      eventId: string
-    }
-  }
-  navigation: any
-}
-
-// Other screen props
-export type AddEventScreenProps = {
-  navigation: any
-  route?: any
-}
-
-export type ManageProgramsScreenProps = {
-  navigation: any
-  route?: any
-}
-
-export type TodaysVibeScreenProps = {
-  navigation: any
-  route?: any
-}
-
-export type AddVibeScreenProps = {
-  navigation: any
-  route?: any
 }
