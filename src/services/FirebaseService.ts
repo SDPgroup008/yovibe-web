1a240a72316c569f1c126667685fb6b97b6b475b
reinol martin <reinolmartin01@gmail.com>
2025-06-30 17:53:17 +0300

    new changes: modified FirebaseService.ts

--- src/services/FirebaseService.ts
+++ src/services/FirebaseService.ts
@@ -45,13 +45,12 @@ class FirebaseService {
   private constructor() {
     console.log("Firebase service initialized")
     this.app = initializeApp({
-      apiKey: "AIzaSyBXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      authDomain: "your-project.firebaseapp.com",
-      projectId: "your-project-id",
-      storageBucket: "your-project.appspot.com",
-      messagingSenderId: "123456789012",
-      appId: "1:123456789012:web:abcdef123456789012345",
-      measurementId: "G-XXXXXXXXXX",
+       apiKey: "AIzaSyCu3hXDaqQ58VvHNQ1On5wxcgaU0CIXCo8",
+  authDomain: "eco-guardian-bd74f.firebaseapp.com",
+  projectId: "eco-guardian-bd74f",
+  storageBucket: "eco-guardian-bd74f.appspot.com",
+  messagingSenderId: "917905910857",
+  appId: "1:917905910857:android:5886ab1db46cec56912398",
     })
     this.authOriginal = getAuth(this.app)
     this.dbOriginal = getFirestore(this.app)
