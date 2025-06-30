c25314abfd058a0dce462dbef7fabccd14fddf7c
reinol martin <reinolmartin01@gmail.com>
2025-06-30 17:53:43 +0300

    new changes: modified FirebaseService.ts

--- src/services/FirebaseService.ts
+++ src/services/FirebaseService.ts
@@ -48,9 +48,9 @@ class FirebaseService {
        apiKey: "AIzaSyCu3hXDaqQ58VvHNQ1On5wxcgaU0CIXCo8",
         authDomain: "eco-guardian-bd74f.firebaseapp.com",
         projectId: "eco-guardian-bd74f",
-  storageBucket: "eco-guardian-bd74f.appspot.com",
-  messagingSenderId: "917905910857",
-  appId: "1:917905910857:android:5886ab1db46cec56912398",
+        storageBucket: "eco-guardian-bd74f.appspot.com",
+        messagingSenderId: "917905910857",
+        appId: "1:917905910857:android:5886ab1db46cec56912398",
     })
     this.authOriginal = getAuth(this.app)
     this.dbOriginal = getFirestore(this.app)
